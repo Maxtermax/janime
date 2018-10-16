54c9ed1a631fab569a8a04101de1389186262655
maxtermax <esnene02@gmail.com>
2018-10-15 20:45:56 -0500

    add server

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT;
+
+//app.use(express.static(__dirname)); // Current directory is root
+app.use(express.static(path.join(__dirname, 'public'))); //  "public" off of current is root
+app.get('/', (req, res) => {
+  res.sendFile('./index.html', 'utf8');
+})
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
