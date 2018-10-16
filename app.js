f5dd7d65e29080df934e6f432d404f99bc29a7ea
maxtermax <esnene02@gmail.com>
2018-10-15 20:53:21 -0500

    fix server

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT;
 //app.use(express.static(__dirname)); // Current directory is root
 app.use(express.static(path.join(__dirname, 'public'))); //  "public" off of current is root
 app.get('/', (req, res) => {
-  res.sendFile('./index.html', 'utf8');
+  res.sendFile(__dirname+'/index.html', 'utf8');
 })
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
