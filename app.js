957938dd8dfc5b85e5bf31ffd18195f50120e928
maxtermax <esnene02@gmail.com>
2018-10-15 21:33:25 -0500

    add sw

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ app.use(express.static(path.join(__dirname, 'public'))); //  "public" off of cur
 app.get('/', (req, res) => {
   res.sendFile(__dirname+'/index.html', 'utf8');
 })
+
+app.get('/sw.js', (req, res) => {
+  res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+  res.header('Expires', '-1');
+  res.header('Pragma', 'no-cache');
+  res.sendFile(__dirname+'/sw.js');
+})
+
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
--- src/js/components/smart/App.js
+++ src/js/components/smart/App.js
@@ -6,6 +6,7 @@ import Main from '../dumb/Main.js'
 import LoadBar from '../dumb/LoadBar.js'
 import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
 const Evolution = require('../../evolution.js')
+var serviceWorker;
 
 const Theme = createMuiTheme({
   palette: {
@@ -27,6 +28,58 @@ const Theme = createMuiTheme({
 class App extends React.Component {
   constructor() {
     super();
+  }
+  componentDidMount() {
+        if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/sw.js').then(function (registration) {
+       // self.tryPushNotification();
+        if (registration.installing) {
+          serviceWorker = registration.installing;
+          console.log('installing');
+          //self.setState({ loading: true });
+        } else if (registration.waiting) {
+          serviceWorker = registration.waiting;
+          //self.setState({ loading: true });
+          console.log('waiting');
+        } else if (registration.active) {
+          serviceWorker = registration.active;
+          console.log('active');
+          //setTimeout(() => self.setState({ loading: false }), 500);
+        }
+
+        if (serviceWorker) {
+          console.log('current state ', serviceWorker.state);
+          if (serviceWorker.state === 'activated') {
+            //setTimeout(() => this.setState({ loading: false }), 1000);
+          }
+          serviceWorker.addEventListener('statechange', function (e) {
+            console.log('current state ', serviceWorker.state);
+            if(e.target.state === 'activated') {
+              console.log('activated')
+              //this.setState({ loading: false });
+            }
+            if (e.target.state === 'installing') {
+              console.log('installing')
+              //this.setState({ loading: true });
+            }
+            if (e.target.state === 'redundant') {
+              console.log('new sw replace old sw');
+              //this.setState({ loading: true });
+            }
+          });
+        }
+
+      }).catch(function (err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+        //this.setState({ loading: false });
+      });
+
+      navigator.serviceWorker.addEventListener('controllerchange', () => {
+        console.log("NUEVO SERVICE WORKER LISTO PARA TOMAR EL CONTROL")
+        setTimeout(() => window.location.reload(), 500);
+      });
+    }
   }
   searchAnime(value) {
     this.props.dispatch({ type: 'SEARCH_ANIME', payload: value })
